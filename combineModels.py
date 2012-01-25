3602ea3dc16a377d706b683a6d3ca6c4a1454e08
Katie Edmonds <betainverse@gmail.com>
2012-01-25 09:55:35 -0500

    added combineModels.py

---
+++ combineModels.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from sys import argv, stdout
+
+#files = argv[1:]
+# Copy list of best structures from refine_##_h2.pdb.stats
+# Then use ^x-r k to remove the other columns, use find-replace to add commas and quotes, and adjust tabs manually
+files = ['refine_62_h2.pdb',
+    'refine_61_h2.pdb',
+    'refine_88_h2.pdb',
+    'refine_31_h2.pdb',
+    'refine_60_h2.pdb',
+    'refine_17_h2.pdb',
+    'refine_75_h2.pdb',
+    'refine_15_h2.pdb',
+    'refine_82_h2.pdb',
+    'refine_2_h2.pdb',
+    'refine_30_h2.pdb',
+    'refine_89_h2.pdb',
+    'refine_20_h2.pdb',
+    'refine_94_h2.pdb',
+    'refine_63_h2.pdb',
+    'refine_74_h2.pdb',
+    'refine_41_h2.pdb',
+    'refine_83_h2.pdb',
+    'refine_7_h2.pdb',
+    'refine_45_h2.pdb']
+
+model=1
+for file in files:
+    print "MODEL %8d" % model
+    stdout.write(open(file).read())
+    print "ENDMDL"
+    model += 1
+    pass
+
+# Is it necessary to add TER fields and chain IDs?
